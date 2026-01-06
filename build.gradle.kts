88b290546cf43af99001d2f9c511885f6e7b17a7
snowykte0426 <snowykte0426@naver.com>
2026-01-06 19:44:32 +0900

    add: update Java source and target compatibility to version 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,8 +12,8 @@ java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(25)
     }
-    sourceCompatibility = JavaVersion.VERSION_13
-    targetCompatibility = JavaVersion.VERSION_13
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
     withSourcesJar()
     withJavadocJar()
 }
