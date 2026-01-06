cdb12094c16ece5b8ccad45d84ac86dcc7ed625f
snowykte0426 <snowykte0426@naver.com>
2026-01-06 18:53:43 +0900

    update: configure Gradle for Java 13 and add Maven publishing support

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,20 @@
 plugins {
-    id("java")
+    `java-library`
+    `maven-publish`
 }
 
-group = "kr.kro.amond-server"
-version = "1.0-SNAPSHOT"
+group = "team.themoment"
+version = "1.0"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(25)
+    }
+    sourceCompatibility = JavaVersion.VERSION_13
+    targetCompatibility = JavaVersion.VERSION_13
+    withSourcesJar()
+    withJavadocJar()
+}
 
 repositories {
     mavenCentral()
@@ -17,4 +28,52 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = "the-sdk"
+            version = project.version.toString()
+
+            from(components["java"])
+
+            pom {
+                name = "The SDK"
+                description = "Modular SDK for reusable Spring Boot components"
+                url = "https://github.com/themoment-team/the-sdk"
+
+                licenses {
+                    license {
+                        name = "MIT License"
+                        url = "https://opensource.org/licenses/MIT"
+                    }
+                }
+
+                developers {
+                    developer {
+                        id = "themoment"
+                        name = "themoment-team"
+                        email = "official.themoment.team@gmail.com"
+                    }
+                }
+
+                scm {
+                    connection = "scm:git:git://github.com/themoment-team/the-sdk.git"
+                    developerConnection = "scm:git:ssh://github.com:themoment-team/the-sdk.git"
+                    url = "https://github.com/themoment-team/the-sdk"
+                }
+            }
+        }
+    }
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+    options.release.set(13)
+}
+
+tasks.withType<Javadoc> {
+    options.encoding = "UTF-8"
 }
