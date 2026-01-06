f99cf09565c26bd6db336b73bc6b11df7607f75a
snowykte0426 <snowykte0426@naver.com>
2026-01-06 23:58:49 +0900

    add: increment project version to 1.0-M7 and update dependencies to use api configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "team.themoment"
-version = "1.0-M6"
+version = "1.0-M7"
 
 java {
     toolchain {
@@ -24,22 +24,21 @@ repositories {
 
 dependencies {
     // Kotlin
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    api("org.jetbrains.kotlin:kotlin-reflect")
+    api("org.jetbrains.kotlin:kotlin-stdlib")
 
     // Spring Boot
-    implementation("org.springframework.boot:spring-boot-starter-web:3.4.1")
-    implementation("org.springframework.boot:spring-boot-autoconfigure:3.4.1")
-    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor:3.4.1")
+    api("org.springframework.boot:spring-boot-starter-web:3.4.1")
+    api("org.springframework.boot:spring-boot-autoconfigure:3.4.1")
 
     // Swagger/OpenAPI
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.15")
+    api("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.15")
 
     // Jackson
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.18.2")
+    api("com.fasterxml.jackson.module:jackson-module-kotlin:2.18.2")
 
     // Logging
-    implementation("org.slf4j:slf4j-api:2.0.16")
+    api("org.slf4j:slf4j-api:2.0.16")
 
     // Testing
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
