19849725c538052145800e3154d90625972fd5f0
Kim Tae Eun <snowykte0426@naver.com>
2026-01-18 01:15:44 +0900

    add: include enabled property in ResponseProperties configuration

--- src/main/kotlin/team/themoment/sdk/config/ResponseProperties.kt
+++ src/main/kotlin/team/themoment/sdk/config/ResponseProperties.kt
@@ -4,6 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(prefix = "sdk.response")
 data class ResponseProperties(
+    val enabled: Boolean = true,
     val notWrappingUrls: List<String> = listOf(
         "/v3/api-docs/**",
         "/v3/api-docs",
